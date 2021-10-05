f4fbbd66bb59dea7872a33b3fdee9e0186a3e2e9
Craig Hackney <iratahack@digitalxfer.com>
2021-10-05 09:30:49 -0700

    Do something a little more interesting.

--- Makefile
+++ Makefile
@@ -8,10 +8,10 @@ gb: test.gb
 cpc:test.dsk
 
 %.dsk: %.c
-	zcc +cpc -lcpcfs -lm -subtype=dsk -create-app -o test.bin test.c
+	zcc +cpc -lcpcfs -lm -subtype=dsk -create-app -o test.bin test.c -DCPC
 
 %.gb: %.c
-	zcc +gb $< -o $@ -create-app
+	zcc +gb $< -o $@ -O0 -create-app -DGB
 
 clean:
 	rm -f *.bin *.tap *.gb
--- test.c
+++ test.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
-
-
+#ifdef CPC
+#include <cpc.h>
+#else
+#include <arch/gb/gb.h>
+#endif
 
 int main(void)
 {
+#ifdef CPC
+	cpc_setmode(2);
+	cpc_ClrScr();
+	cpc_SetBorder(0);
+	cpc_SetInk(0, 0);
+	cpc_SetInk(1, 26);
+
 	printf("Hello World!\n");
+#else
+	int a = 0;
+	int n;
 
-	while(1);
-	return(0);
+	printf("Hello World!\n");
+	while (1)
+	{
+		move_bkg(a--, 0);
+		for (n = 0; n < 255; n++)
+			;
+	}
+#endif
+
+	while (1)
+		;
+	return (0);
 }
-
