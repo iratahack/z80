1fa96af9cfda6d6d26e2b35eaaec7f868b323b43
Craig Hackney <iratahack@digitalxfer.com>
2021-10-15 23:26:21 -0700

    Caller needs to registers.

--- cpc_sprite/border.asm
+++ cpc_sprite/border.asm
@@ -1,9 +1,7 @@
         public  border
         section CODE_0
 border:
-        push    bc
         ld      bc, 0x7F10
         out     (c), c
         out     (c), a
-        pop     bc
         ret
