bb41e4378505cd62bd393f75aefdb5b643fd8e06
Craig Hackney <iratahack@digitalxfer.com>
2021-10-18 22:21:46 -0700

    Testing char display algorithm.

--- cpc_sprite/BIOS/puts.asm
+++ cpc_sprite/BIOS/puts.asm
@@ -29,6 +29,7 @@ updateCursor:
         ld      a, b
         cp      39                      ; Screen character width in mode 1
         jr      nz, nextCol
+nl:
         ld      b, 0                    ; Reset to left size of screen
         ld      a, c
         cp      24                      ; screen character height in mode 1
@@ -36,7 +37,7 @@ updateCursor:
         ld      c, 0
         jr      setCursor
 nextRow:
-        inc     c                       ;
+        inc     c                       ; Next row
         jr      setCursor
 nextCol:
         inc     b
@@ -49,6 +50,10 @@ nextCol:
 setCursor:
         ld      (cursorPos), bc
         ret
+
+newLine:
+        ld      bc, (cursorPos)
+        jr      nl
 
         ;
         ; Display a string.
@@ -79,14 +84,19 @@ puts:
         ;   A = Character to display
         ;   E = Pen
 putc:
-        cp      ' '
+        cp      13                      ; Handle CR/LF
+        push    af
+        call    z, newLine
+        pop     af
+        ret     z
+
+        sub     ' '                     ; Characters below 0x20 are not displayed
         ret     c
 
-        ld      IX, mode1PenMask
+        ld      IX, penMask
         ld      d, 0x00
         add     IX, de
 
-        sub     ' '
         ld      l, a
         ld      h, 0
         add     hl, hl
@@ -112,7 +122,6 @@ putc:
 
         ld      a, b
         add     a                       ; x2 since 2 bytes per char in mode 1
-
         addde
 
         call    updateCursor
@@ -121,30 +130,61 @@ putc:
 
         ld      b, 8
 nextByte:
-        ld      a, (hl)                 ; Get byte of font data
-        and     0xf0                    ; Mask off lower nibble
-        ld      c, a                    ; Save upper nibble
-        rrca                            ; Move upper nibble to lower nibble
-        rrca
-        rrca
-        rrca
-        or      c                       ; Or upper nibble back in
-        and     (IX+0)                  ; Pen mask
-        ld      (de), a                 ; Write to the screen
+IF  1
+        ld      a, (hl)                 ; [7] Get byte of font data
+        and     0xf0                    ; [7] Mask off lower nibble
+        ld      c, a                    ; [4] Save upper nibble
+        rrca                            ; [4] Move upper nibble to lower nibble
+        rrca                            ; [4]
+        rrca                            ; [4]
+        rrca                            ; [4]
+        or      c                       ; [4] Or upper nibble back in
+        and     (IX+0)                  ; [19] Pen mask
+        ld      (de), a                 ; [7] Write to the screen
 
-        inc     e                       ; Next screen address
+        inc     e                       ; [4] Next screen address
 
-        ld      a, (hl)                 ; Get byte of font data
-        and     0x0f                    ; Mask off upper nibble
-        ld      c, a                    ; Save lower nibble
-        rlca                            ; Move lower nibble to upper nibble
-        rlca
-        rlca
-        rlca
-        or      c                       ; Or lower nibble back in
-        and     (IX+0)                  ; Pen mask
-        ld      (de), a                 ; Write to the screen
+        ld      a, (hl)                 ; [7] Get byte of font data
+        and     0x0f                    ; [7] Mask off upper nibble
+        ld      c, a                    ; [4] Save lower nibble
+        rlca                            ; [4] Move lower nibble to upper nibble
+        rlca                            ; [4]
+        rlca                            ; [4]
+        rlca                            ; [4]
+        or      c                       ; [4] Or lower nibble back in
+        and     (IX+0)                  ; [19] Pen mask
+        ld      (de), a                 ; [7] Write to the screen
+ELSE
+        push    bc                      ; [11]
 
+        ld      b, 2                    ; [7]
+        ld      a, (hl)                 ; [7] Get byte of font data
+nextNibble:
+        push    bc                      ; [11]
+
+        ld      c, a                    ; [4]
+        ld      b, 4                    ; [7]
+        ld      IY, pixelMask           ; [14]
+        xor     a                       ; [4]
+nextPixel:
+        rlc     c                       ; [8]
+        jr      nc, noPixel             ; [12/7]
+        or      (IY+0)                  ; [19]
+noPixel:
+        inc     IY                      ; [10]
+        djnz    nextPixel               ; [13/8]
+
+        and     (IX+0)                  ; [19] Pen mask
+        ld      (de), a                 ; [7] Write to the screen
+        inc     e                       ; [4] Next screen address
+        ld      a, c                    ; [4]
+
+        pop     bc                      ; [10]
+        djnz    nextNibble              ; [13/8]
+
+        pop     bc                      ; [10]
+        dec     e
+ENDIF
         dec     e                       ; Previous screen address
         ; Next screen line down
         ld      a, 0x08
@@ -157,7 +197,15 @@ nextByte:
         ret
 
         section RODATA_0
-mode1PenMask:
+        ; Pen and pixel masks for video mode 1
+IF  0
+pixelMask:
+        db      %10001000
+        db      %01000100
+        db      %00100010
+        db      %00010001
+ENDIF
+penMask:
         db      0x00                    ; Pen 0
         db      0x0f                    ; Pen 1
         db      0xf0                    ; Pen 2
