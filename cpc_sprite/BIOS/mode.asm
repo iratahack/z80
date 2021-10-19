4bc0c99ab77bb94a6dc06b8c1a616a7007cf7d4d
Craig Hackney <iratahack@digitalxfer.com>
2021-10-19 09:04:09 -0700

    Initial commit.

---
+++ cpc_sprite/BIOS/mode.asm
@@ -0,0 +1,16 @@
+        public  setMode
+
+        section CODE_0
+
+        ; Set the video mode.
+        ;
+        ; Entry:
+        ;   A = Video mode 0-3
+        ;
+        ; Exit:
+        ;   B, A are corrupted
+setMode:
+        ld      b, 0x7f
+        or      0x8c				    ; Select register #2, disable Hi and Lo ROM's
+        out     (c), a
+        ret
