b198cb2c1cff7c8983bd53ecd8ef295d46b35bf4
Craig Hackney <iratahack@digitalxfer.com>
2021-10-24 21:00:55 -0700

    Makefile cleanup.

--- cpc_test/test.c
+++ cpc_test/test.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-#ifdef CPC
 #include <cpc.h>
-#else
-#include <arch/gb/gb.h>
-#include <arch/gb/drawing.h>
-#endif
 
 int main(void)
 {
-#ifdef CPC
-	cpc_setmode(2);
+	cpc_setmode(0);
 	cpc_SetInk(0, 0);
 	cpc_SetInk(1, 26);
 
@@ -21,17 +15,6 @@ int main(void)
 
 	while (1)
 		;
-#else
-	int a = 0;
-	int n;
-	color(BLACK, WHITE, OR);
-	circle(GRAPHICS_WIDTH / 2, GRAPHICS_HEIGHT / 2, GRAPHICS_WIDTH / 4, M_NOFILL);
-	while (1)
-	{
-		move_bkg(a--, 0);
-		__asm__("halt");
-	}
-#endif
 
 	return (0);
 }
--- gb_test/test.c
+++ gb_test/test.c
@@ -1,27 +1,9 @@
 #include <stdio.h>
-#ifdef CPC
-#include <cpc.h>
-#else
 #include <arch/gb/gb.h>
 #include <arch/gb/drawing.h>
-#endif
 
 int main(void)
 {
-#ifdef CPC
-	cpc_setmode(2);
-	cpc_SetInk(0, 0);
-	cpc_SetInk(1, 26);
-
-	__asm__("halt");
-	cpc_ClrScr();
-	cpc_SetBorder(0);
-
-	printf("Hello World!\n");
-
-	while (1)
-		;
-#else
 	int a = 0;
 	int n;
 	color(BLACK, WHITE, OR);
@@ -31,7 +13,6 @@ int main(void)
 		move_bkg(a--, 0);
 		__asm__("halt");
 	}
-#endif
 
 	return (0);
 }
