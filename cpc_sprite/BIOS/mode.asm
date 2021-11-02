db613d0b2e1142a2a2591bd326ff82de5c151674
Craig Hackney <iratahack@digitalxfer.com>
2021-11-02 08:52:31 -0700

    Update the screen width based on which mode is selected.

--- cpc_sprite/BIOS/mode.asm
+++ cpc_sprite/BIOS/mode.asm
@@ -1,5 +1,8 @@
+        include "BIOS/macros.inc"
+
         public  setMode
         public  screenMode
+        public  screenWidth
 
         section CODE_0
 
@@ -12,11 +15,26 @@
         ;   B, A are corrupted
 setMode:
         ld      (screenMode), a
+        ld      b, a
+        push    hl
+        ld      hl, widths
+        addhl
+        ld      a, (hl)
+        ld      (screenWidth), a
+        pop     hl
+        ld      a, b
+
         ld      b, 0x7f
         or      0x8c                    ; Select register #2, disable Hi and Lo ROM's
         out     (c), a
         ret
 
+        section RODATA_0
+widths:                                 ; Screen width in characters for modes 0-2
+        db      20, 40, 80
+
         section BSS_0
 screenMode:
+        ds      1
+screenWidth:
         ds      1
