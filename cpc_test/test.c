6c56a2d1e01175525162e54f804c65cdc3e50775
Craig Hackney <iratahack@digitalxfer.com>
2021-11-12 19:50:03 -0800

    Mild cleanup.

--- cpc_test/test.c
+++ cpc_test/test.c
@@ -2,14 +2,17 @@
 #include <cpc.h>
 #include <compress/zx0.h>
 
+// Address of screen memory
+extern unsigned char _BANK_3_head;
+
 __asm
     section RODATA
 _image:
     binary "title.scr.zx0"
 __endasm
 
+// Palette for The Enterprise
 unsigned char palette[] = {00, 06, 18, 26};
-
 
 int main(void)
 {
@@ -18,7 +21,7 @@ int main(void)
 	for(int n=0; n<4; n++)
 	    cpc_SetInk(n, palette[n]);
 
-	dzx0_standard(&image, 0xc000);
+	dzx0_standard(&image, &_BANK_3_head);
 
     printf("Hello World!\n");
 
