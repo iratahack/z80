2fea8161cb83e5556ee7c1cbedd2b2cddb5d4ab2
Craig Hackney <iratahack@digitalxfer.com>
2021-12-26 23:01:57 -0800

    Example to color the screen.

--- zxn_test/sprite.asm
+++ zxn_test/sprite.asm
@@ -1,48 +1,63 @@
         public  _main
-        section code_user
+        org     0x8000
 _main:
         di
+        nextreg 0x07, 0x00              ; CPU speed (0=3.5Mhz, 1=7Mhz, 2=14 Mhz )
+
+        ld      sp, 0x8000              ; Set stack pointer
+
+		; Clear regular screen bitmap area
+        ld      hl, 0x4000
+        ld      (hl), 0
+        ld      de, 0x4001
+        ld      bc, 6144
+        ldir
+
         nextreg 0x43, 0x10              ; Layer 2 of 1st palette
 
         nextreg 0x40, 0x00              ; Palette index 0
-
-        ld      b, 4
-        ld      hl, palette
-ploop:
-        ld      a, (hl)
-        inc     hl
-        nextreg 0x41, a                 ; Send color
-        djnz    ploop
+        ld      b, 0x00
+        xor     a
+paletteLoop:
+        nextreg 0x41, a
+        inc     a
+        djnz    paletteLoop
 
         nextreg 0x16, 0x00              ; Set X scroll to 0
         nextreg 0x17, 0x00              ; Set Y scroll to 0
 
         nextreg 0x18, 0x00              ; X1 - 0
         nextreg 0x18, 0xff              ; X2 - 255
-        nextreg 0x18, 0x00              ; Y1 - 0 
+        nextreg 0x18, 0x00              ; Y1 - 0
         nextreg 0x18, 0xbf              ; Y2 - 191
 
+        ld      b, 3
         ld      a, 0x03                 ; BB--P-VW
+next3rd:
+        push    bc
+
         ld      bc, 0x123b
         out     (c), a
 
-        nextreg 0x07, 0x02              ; CPU speed (0=3.5Mhz, 1=7Mhz, 2=14 Mhz )
+        push    af
+        call    fill
+        pop     af
 
+        add     0x40
+
+        pop     bc
+        djnz    next3rd
+
+        halt
+
+fill:
         ld      hl, 0x0000
         ld      bc, 0x4000
-colorsAgain:
+nxt:
         dec     bc
         ld      (hl), c
         inc     hl
         ld      a, b
         or      c
-        jr      nz, colorsAgain
-
-        halt
-
-        section rodata_user
-palette:
-        db      %00000001
-        db      %11111100
-        db      %00011111
-        db      %11100000
+        jr      nz, nxt
+        ret
