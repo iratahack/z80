6658f0c2e3cec48b35e0ab2aa18e7afe590e3d4a
Craig Hackney <iratahack@digitalxfer.com>
2022-01-25 19:51:10 -0800

    Basic bitmap and text display for the SMS.

--- sms_test/test.c
+++ sms_test/test.c
@@ -1,49 +1,98 @@
 #include <sms.h>
 #include <stdio.h>
 
-unsigned char pal1[] = {0x00, 0x20, 0x08, 0x28, 0x02, 0x22, 0x0A, 0x2A,
-						0x15, 0x35, 0x1D, 0x3D, 0x17, 0x37, 0x1F, 0x3F};
-
-unsigned char pal2[] = {0x00, 0x03, 0x08, 0x28, 0x02, 0x22, 0x0A, 0x2A,
-						0x15, 0x35, 0x1D, 0x3D, 0x17, 0x37, 0x1F, 0x3F};
+static unsigned char textPal[] = {0x00, 0x3f};
 
 #asm
+section rodata_user
 _titlePal:
-	include "title_pal.inc"
+    include "title_pal.inc"
 _titlePalEnd:
 _tiles:
-	include "tiles.inc"
+    include "tiles.inc"
 _tilesEnd:
 _tileMap:
-	include "tilemap.inc"
+    include "tilemap.inc"
 _tileMapEnd:
+section code_compiler
 #endasm
 
-	void
-	main()
+extern unsigned char titlePal;
+extern unsigned char titlePalEnd;
+extern unsigned char tiles;
+extern unsigned char tilesEnd;
+extern unsigned int tileMap;
+
+void __FASTCALL__ setVRAMAddr(unsigned int addr)
 {
-#if 0
-	int y = 0;
+#asm
+    push    af
+    ld      a,l
+    out     ($bf),a
+    ld      a,h
+    or      $40
+    out     ($bf),a
+    pop     af
+#endasm
+}
 
-	clear_vram();
-	load_tiles(standard_font, 0, 255, 1);
-	load_palette(pal1, 0, 16);
-	load_palette(pal2, 16, 16);
-	set_vdp_reg(VDP_REG_FLAGS1, VDP_REG_FLAGS1_BIT7 | VDP_REG_FLAGS1_SCREEN);
+void __FASTCALL__ clearScreen(void)
+{
+#asm
+    push    af
+    push    bc
+    push    hl
 
-	printf("Hello, stdio!\nIs it working?\nI hope so.\n");
-	printf("01234567890123456789012345678901\n");
-	gotoxy(5, 5);
-	printf("Hello, gotoxy(%d, %d)!", 5, 5);
-#else
-	clear_vram();
-	load_tiles((unsigned char *)&tiles, 0, ((unsigned char *)&tilesEnd - (unsigned char *)&tiles) / 32, 4);
-	load_palette((unsigned char *)&titlePal, 0, ((unsigned char *)&titlePalEnd - (unsigned char *)&titlePal));
-	set_bkg_map((unsigned int *)&tileMap, 0, 0, 32, 24);
-	set_vdp_reg(VDP_REG_FLAGS1, VDP_REG_FLAGS1_BIT7 | VDP_REG_FLAGS1_SCREEN);
-#endif
-	for (;;)
-	{
-		wait_vblank_noint();
-	}
+    ld      hl, 0x3800
+    ld      a,l
+    out     ($bf),a
+    ld      a,h
+    or      $40
+    out     ($bf),a
+
+    ld      bc, 0x0003
+l1:
+    ld      a, ' '          ; SPACE
+    out     ($be), a        ; Character number
+    xor     a
+    out     ($be), a        ; Attribute number
+
+    djnz    l1
+
+    dec     c
+    jr      nz, l1
+
+    pop     hl
+    pop     bc
+    pop     af
+#endasm
+}
+
+void main()
+{
+    int n;
+
+    // Clear 16KB of VRAM
+    clear_vram();
+
+    load_tiles(&tiles, 0, (&tilesEnd - &tiles) / 32, 4);
+    load_palette(&titlePal, 0, &titlePalEnd - &titlePal);
+    set_bkg_map(&tileMap, 0, 0, 32, 24);
+
+    // Enable screen and frame interrupts
+    set_vdp_reg(VDP_REG_FLAGS1, VDP_REG_FLAGS1_SCREEN | VDP_REG_FLAGS1_VINT);
+
+    // Wait for 5 seconds
+    for (n=0; n<60*5; n++)
+    {
+        __asm__("halt");
+    }
+
+    set_vdp_reg(VDP_REG_FLAGS1, 0);
+    load_palette(textPal, 0, 2);
+    load_tiles(standard_font, 0, 256, 1);
+    clearScreen();
+    gotoxy(13, 11);
+    printf("Done!\n");
+    set_vdp_reg(VDP_REG_FLAGS1, VDP_REG_FLAGS1_SCREEN | VDP_REG_FLAGS1_VINT);
 }
