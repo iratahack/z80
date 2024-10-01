94c79377a6be5f2fe80936e20a4c8dc246c89618
Craig Hackney <iratahack@digitalxfer.com>
2024-09-30 22:41:10 -0700

    Code style update.

--- aquarius-plus-isr/main.c
+++ aquarius-plus-isr/main.c
@@ -1,32 +1,33 @@
-#include <stdio.h>
+#include <games.h>
 #include <interrupt.h>
 #include <intrinsic.h>
-#include <games.h>
+#include <stdio.h>
 
-isr_t ISR( void )
+isr_t ISR(void)
 {
-	M_PRESERVE_MAIN;
+    M_PRESERVE_MAIN;
 
-	// Do something really cool here!
+    // Do something really cool here!
 
-	M_RESTORE_MAIN;
+    M_RESTORE_MAIN;
 }
 
 void main(void)
 {
-	int	a;
+    int a;
 
-	// Setup im1 interrupts
-	im1_init();
+    // Setup im1 interrupts
+    im1_init();
 
-	// Install user ISR
-	im1_install_isr(ISR);
+    // Install user ISR
+    im1_install_isr(ISR);
 
-	do {
-		intrinsic_halt();
-		a = joystick(1);
-		printf("0x%02x\n", a);
-		if ( a & 0x10 )
-			return(0);
-	} while(1);
+    do
+    {
+        intrinsic_halt();
+        a = joystick(1);
+        printf("0x%02x\n", a);
+        if (a & 0x10)
+            return;
+    } while (1);
 }
